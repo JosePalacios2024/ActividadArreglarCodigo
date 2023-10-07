de5a9fddab735f14fa5130a1df74ffcbed520117
JosePalacios2024 <jose.palacios2024@hilasal.superate.org.sv>
2023-10-06 18:49:34 -0600

    :bug: Arreglo de errores

--- src/chef/createChef.js
+++ src/chef/createChef.js
@@ -7,9 +7,9 @@ function addChef() {
     const experience = document.getElementById("experience");
     const area = document.getElementById("area");
     const cv = document.getElementById("cv");
-    const job1 = document.getElementById("job1").alue;
+    const job1 = document.getElementById("job1").value;
     const job2 = document.getElementById("job2");
-    const job3 = document.getElementById("job3");
+    const job3 = document.getElementById("job3").value;
 
     const newChef = new Chef({
         name: name,
@@ -24,7 +24,7 @@ function addChef() {
 
     
 
-    let chefs = JSON.parse(localStorage.get('chefs')) || [];
+    let chefs = JSON.parse(localStorage.getItem('chefs')) || [];
     chefs.push(newChef);
     localStorage.setItem('chefs', JSON.stringify(chefs));
 
